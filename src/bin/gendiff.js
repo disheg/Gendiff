a5f0a5d0166e6e591b6269fb79d3d5172fbc1b27
Disheg <disheg@gmail.com>
2020-06-01 17:45:55 +0200

    step 3

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,7 +6,9 @@ program
   .version('0.0.1')
   .usage('[options] <filepath1> <filepath2>')
   .command('gendiff <filepath1> <filepath2>')
-  .action(function(file1, file2) { console.log(gendiff(file1, file2)) })
+  .action((file1, file2) => {
+    console.log(gendiff(file1, file2));
+  })
   .description('Compares two configuration files and shows a difference')
   .option('-f, --format [type]', 'output format')
   .parse(process.argv);
--- src/index.js
+++ src/index.js
@@ -2,39 +2,41 @@ import fs from 'fs';
 import path from 'path';
 
 const gendiff = (file1, file2) => {
-  const result = [];
   const currentDirectory = process.cwd();
   const objFileOne = JSON.parse(
     fs.readFileSync(
-      path.resolve(currentDirectory, file1)
-    )
+      path.resolve(currentDirectory, file1),
+    ),
   );
   const objFileTwo = JSON.parse(
     fs.readFileSync(
-      path.resolve(currentDirectory, file2)
-    )
+      path.resolve(currentDirectory, file2),
+    ),
   );
   const keysOne = Object.keys(objFileOne);
   const keysTwo = Object.keys(objFileTwo);
 
-  keysOne.map((element) => {
+  const resultFileOne = keysOne.reduce((acc, element) => {
     if (keysTwo.includes(element)) {
       if (objFileOne[element] === objFileTwo[element]) {
-        result.push([`  ${element}: ${objFileOne[element]}`]);
+        return [...acc, `  ${element}: ${objFileOne[element]}`];
       }
       if (objFileOne[element] !== objFileTwo[element]) {
-        result.push([`+ ${element}: ${objFileTwo[element]}`]);
-        result.push([`- ${element}: ${objFileOne[element]}`]);
+        return [...acc, `+ ${element}: ${objFileTwo[element]}`, `- ${element}: ${objFileOne[element]}`];
       }
     } else {
-      result.push([`- ${element}: ${objFileOne[element]}`]);
+      return [...acc, `- ${element}: ${objFileOne[element]}`];
     }
-  });
-  keysTwo.map((element) => {
+    return acc;
+  }, []);
+  const resultFileTwo = keysTwo.reduce((acc, element) => {
     if (!keysOne.includes(element)) {
-      result.push([`+ ${element}: ${objFileTwo[element]}`]);
+      return [...acc, `+ ${element}: ${objFileTwo[element]}`];
     }
-  })
+    return acc;
+  }, []);
+
+  const result = [...resultFileOne, ...resultFileTwo];
   return `{\n  ${result.join('\n  ')}\n}`;
 };
 
