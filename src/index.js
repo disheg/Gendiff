d77ef10f96600cf99807c8879d149d94187ccb44
Disheg <disheg@gmail.com>
2020-06-09 11:12:21 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -8,24 +8,30 @@ const makeChangedObj = (objFileOne, objFileTwo) => {
   const keys = _.union(keysOne, keysTwo);
 
   const result = keys.map((key) => {
+    let type;
+    let value = objFileOne[key];
     if (!_.has(objFileTwo, key)) {
-      return { key, value: objFileOne[key], type: 'deleted' };
-    }
-    if (_.isObject(objFileOne[key]) && _.isObject(objFileTwo[key])) {
+      type = 'deleted';
+    } else if (_.isObject(objFileOne[key]) && _.isObject(objFileTwo[key])) {
       return { key, value: makeChangedObj(objFileOne[key], objFileTwo[key]) };
-    }
-    if (objFileOne[key] === objFileTwo[key]) {
-      return { key, value: objFileOne[key], type: 'unchanged' };
-    }
-    if (!_.has(objFileOne, key)) {
-      return { key, value: objFileTwo[key], type: 'added' };
+    } else if (objFileOne[key] === objFileTwo[key]) {
+      type = 'unchanged';
+    } else if (!_.has(objFileOne, key)) {
+      type = 'added';
+      value = objFileTwo[key];
+    } else if (_.has(objFileOne, key) && _.has(objFileTwo, key)) {
+      return {
+        key,
+        currentValue: objFileTwo[key],
+        beforeValue: objFileOne[key],
+        type: 'changed',
+      };
     }
     return {
       key,
-      currentValue: objFileTwo[key],
-      beforeValue: objFileOne[key],
-      type: 'changed',
-    };
+      value,
+      type,
+    }
   });
   return result;
 };
