a6c254a71b1eb8de0230d7a9777b8e5b7b462e62
Disheg <disheg@gmail.com>
2020-06-09 11:58:07 +0200

    refactoring

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import _ from 'lodash';
 import stylish from './modules/stylish.js';
 import parserToObj from './modules/parsers.js';
 
-const makeType = (key, obj, type) => {
+const makeType = (key, value, type = null) => {
   const result = {
     key,
-    value: obj[key],
+    value,
     type,
   };
   return result;
@@ -17,13 +17,13 @@ const makeChangedObj = (objFileOne, objFileTwo) => {
   const keys = _.union(keysOne, keysTwo);
   const result = keys.reduce((acc, key) => {
     if (!_.has(objFileTwo, key)) {
-      acc.push(makeType(key, objFileOne, 'deleted'));
+      acc.push(makeType(key, objFileOne[key], 'deleted'));
     } else if (_.isObject(objFileOne[key]) && _.isObject(objFileTwo[key])) {
-      acc.push({ key, value: makeChangedObj(objFileOne[key], objFileTwo[key]) });
+      acc.push(makeType(key, makeChangedObj(objFileOne[key], objFileTwo[key])));
     } else if (objFileOne[key] === objFileTwo[key]) {
-      acc.push(makeType(key, objFileOne, 'unchanged'));
+      acc.push(makeType(key, objFileOne[key], 'unchanged'));
     } else if (!_.has(objFileOne, key)) {
-      acc.push(makeType(key, objFileTwo, 'added'));
+      acc.push(makeType(key, objFileTwo[key], 'added'));
     } else {
       acc.push({
         key,
