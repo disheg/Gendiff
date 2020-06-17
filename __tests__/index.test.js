b37a7110c636a105a89e4aa0a11b56ff90dc777f
play.js <git@gituser.com>
2020-06-17 09:12:24 +0200

    refactoring

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,99 +1,15 @@
 import { test, expect } from '@jest/globals';
 import gendiff from '../src/index.js';
+import { resultDefault, resultPlainTree, resultJSON } from './fixtures/result.js';
 
-const result1 = `{
-    host: hexlet.io
-  + timeout: 20
-  - timeout: 50
-  - proxy: 123.234.53.22
-  - follow: false
-  + verbose: true
-}`;
-const result1rec = `{
-    common: {
-        setting1: Value 1
-      - setting2: 200
-      + setting3: {
-            key: value
-        }
-      - setting3: true
-        setting6: {
-            key: value
-          + ops: vops
-        }
-      + follow: false
-      + setting4: blah blah
-      + setting5: {
-            key5: value5
-            kor: {
-                ser: ses
-            }
-        }
-    }
-    group1: {
-      + baz: bars
-      - baz: bas
-        foo: bar
-      + nest: str
-      - nest: {
-            key: value
-        }
-    }
-  - group2: {
-        abc: 12345
-    }
-  + group3: {
-        fee: 100500
-    }
-}`;
-const result2 = `{
-    name: Petya
-  + age: 32
-  - age: 30
-    city: Stockholm
-  + job: Microsoft
-}`;
-test('from JSON', () => {
-  expect(gendiff('__tests__/fixtures/before1.json', '__tests__/fixtures/after1.json')).toEqual(result1rec);
-  expect(gendiff('__tests__/fixtures/before2.json', '__tests__/fixtures/after2.json')).toEqual(result2);
-});
-test('from YML', () => {
-  expect(gendiff('__tests__/fixtures/before1.yml', '__tests__/fixtures/after1.yml')).toEqual(result1);
-  expect(gendiff('__tests__/fixtures/before2.yml', '__tests__/fixtures/after2.yml')).toEqual(result2);
-});
-test('from INI', () => {
-  expect(gendiff('__tests__/fixtures/before1.ini', '__tests__/fixtures/after1.ini')).toEqual(result1);
+test('default Format', () => {
+  expect(gendiff('__tests__/fixtures/beforeDefault.ini', '__tests__/fixtures/afterDefault.yml')).toEqual(resultDefault);
 });
 
-
-const result1Plain = `Property 'timeout' was changed from '50' to '20'
-Property 'proxy' was deleted
-Property 'follow' was deleted
-Property 'verbose' was added with value: 'true'`;
-const result1PlainTree = `Property 'common.setting2' was deleted
-Property 'common.setting3' was changed from 'true' to [complex value]
-Property 'common.setting6.ops' was added with value: 'vops'
-Property 'common.follow' was added with value: 'false'
-Property 'common.setting4' was added with value: 'blah blah'
-Property 'common.setting5' was added with value: [complex value]
-Property 'group1.baz' was changed from 'bas' to 'bars'
-Property 'group1.nest' was changed from [complex value] to 'str'
-Property 'group2' was deleted
-Property 'group3' was added with value: [complex value]`;
-
-test('from JSON Plain', () => {
-  expect(gendiff('__tests__/fixtures/before1.json', '__tests__/fixtures/after1.json', 'plain')).toEqual(result1PlainTree);
-  expect(gendiff('__tests__/fixtures/before1.ini', '__tests__/fixtures/after1.ini', 'plain')).toEqual(result1Plain);
-});
-test('from YML Plain', () => {
-  expect(gendiff('__tests__/fixtures/before1.yml', '__tests__/fixtures/after1.yml', 'plain')).toEqual(result1Plain);
-});
-test('from INI Plain', () => {
-  expect(gendiff('__tests__/fixtures/before1.ini', '__tests__/fixtures/after1.ini', 'plain')).toEqual(result1Plain);
+test('plain Format', () => {
+  expect(gendiff('__tests__/fixtures/before1.json', '__tests__/fixtures/after1.json', 'plain')).toEqual(resultPlainTree);
 });
 
-const resultJSON = '[{"key":"name","value":"Petya","children":[],"type":"unchanged"},{"key":"age","currentValue":32,"beforeValue":30,"children":[],"type":"changed"},{"key":"city","value":"Stockholm","children":[],"type":"unchanged"},{"key":"job","value":"Microsoft","children":[],"type":"added"}]';
-
-test('from JSON to JSON', () => {
+test('json Format', () => {
   expect(gendiff('__tests__/fixtures/before2.json', '__tests__/fixtures/after2.json', 'json')).toEqual(resultJSON);
 });
