4797ad3e382a234ce8939cb84b339dc98f93ccf8
play.js <git@gituser.com>
2020-06-17 11:03:02 +0200

    fixed bugs

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,7 +3,7 @@ import fs from 'fs';
 import path from 'path';
 import gendiff from '../src/index.js';
 
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const __dirname = path.resolve();
 
 let resultDefault;
 let resultPlainTree;
