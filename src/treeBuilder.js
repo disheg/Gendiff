82f9861fefc7e9e421393432b141d649b614e07b
disheg <disheg@gmail.com>
2020-09-19 10:06:31 +0200

    reduced the cognitive complexity

--- src/treeBuilder.js
+++ src/treeBuilder.js
@@ -3,7 +3,8 @@ import _ from 'lodash';
 const buildTree = (data1, data2) => {
   const keysOne = Object.keys(data1);
   const keysTwo = Object.keys(data2);
-  const keys = _.union(keysOne, keysTwo);
+  //const keys = _.union(keysOne, keysTwo);
+  const keys = [...(new Set([...keysOne, ...keysTwo]))];
   const result = keys.map((key) => {
     if (!_.has(data2, key)) {
       return {
