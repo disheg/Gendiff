d45250be990e146f14a225e704bae66b60a5e3fc
play.js <git@gituser.com>
2021-02-16 08:13:07 +0100

    refactoring

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -6,7 +6,7 @@ const stringify = (data, depth = 0) => {
   if (!_.isObject(data)) {
     return data;
   }
-  const entries = Object.entries(data);
+const entries = Object.entries(data);
   const result = entries.map(([key, value]) => `${indent(depth)}  ${key}: ${stringify(value, depth + 1)}`);
   return `{\n${result.join('\n')}\n  ${indent(depth - 1)}}`;
 };
@@ -15,8 +15,6 @@ const renderStylish = (obj) => {
   const iter = (innerObj, depth = 0) => {
     const output = innerObj.flatMap((element) => {
       switch (element.type) {
-        case 'root':
-          return `{\n${iter(element.children, depth + 1).join('\n')}\n}`;
         case 'unchanged':
           return `${indent(depth)}  ${element.key}: ${stringify(element.value, depth + 1)}`;
         case 'deleted':
@@ -36,6 +34,6 @@ const renderStylish = (obj) => {
     });
     return output;
   };
-  return iter([obj]).join('');
+  return `{\n${iter(obj.children).join('\n')}\n}`.join('');
 };
 export default renderStylish;
